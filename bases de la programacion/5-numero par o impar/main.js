f2d0ac9dd84fc549ab02d8312a2c990ded2ba3b5
Juan Pablo <juanpa_roto@hotmail.com>
2023-01-30 15:19:23 -0600

    ended project 5

--- bases de la programacion/5-numero par o impar/main.js
+++ bases de la programacion/5-numero par o impar/main.js
@@ -1,5 +1,10 @@
-let var1 = parseInt(prompt("Enter a number"));
+let var1 = parseInt(prompt("enter a number"));
 
 for (let i = 1; i <= var1; i++) {
-
+    if (i % 2 === 0) {
+        console.log(`es par ${i}`);
+    }
+    else {
+        console.log(`es impar ${i}`);
+    }
 }
