f226faea1ee98630cf8451ebe2a630f8a975a639
Juan Pablo <juanpa_roto@hotmail.com>
2023-02-18 08:29:00 -0600

    added lection Arrays

---
+++ bases de la programacion/Arrays/1.- Manejo de arreglos/main.js
@@ -0,0 +1,22 @@
+// * QUIERO HACER UN ARRAYPUSH Y USAR EL .FOREACH PARA RECORRERLO Y MOSTRARLO EN PANTALLA
+// * AL IGUAL QUE QUIERO USAR UN FOR CON EL METODO .LENGTH PARA RECORRERLO Y MOSTRARLO EN PANTALLA
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function loop() {
+    for (let i = 1; i < array.length; i++) {
+        array.forEach(element => {
+            console.log(`El elemento es: ${element}`)
+        });
+        console.log(array[i]);
+    }
+}loop();
+
+const button = document.createElement('button');
+button.textContent = 'Click me';
+document.body.appendChild(button);
+
+button.addEventListener('click', () => {
+    array.push(20);
+    console.log(array);
+    loop();
+});
